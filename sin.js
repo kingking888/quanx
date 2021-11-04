7730a134c8c3e30e227aad073c68e769894e16b2
Wbb <cgnbin@qq.com>
2021-11-04 18:29:17 +0800

    Update sin.js

--- sin.js
+++ sin.js
@@ -1,6 +1,18 @@
 //shen i nin
-var obj = JSON.parse($response.body);
-var timestamp = (new Date()).valueOf(); //时间戳,转JSON日期默认是标准时区
-obj.data.records[0]["reportTime"] = new Date(timestamp).toJSON().substr(0, 19).replace('T', ' ');
-obj.data.records[0]["sampleTime"] = new Date(timestamp-11*3600*1000).toJSON().substr(0, 19).replace('T', ' ');
-$done({body: JSON.stringify(obj)});
+
+//时间戳取当天0点
+function get0(timestamp){
+  return timestamp - (timestamp+3600*8)%86400;
+}
+
+//中国时区导出标准时间格式
+function goodtime(time,second=true){
+  time.setMinutes(time.getMinutes()-time.getTimezoneOffset());
+  return time.toJSON().substr(0,second?19:16).replace('T', ' ');
+}
+
+var body = JSON.parse($response.body);
+var timestamp = Math.round((new Date()).valueOf()/1000); //当前时间戳
+body.data.records[0]["reportTime"] = goodtime(new Date((timestamp-2*3600-15*60-22)*1000));
+body.data.records[0]["sampleTime"] = goodtime(new Date((get0(timestamp)-4*3600-25*60-35)*1000));
+$done({body:JSON.stringify(body)});
