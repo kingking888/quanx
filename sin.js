5d0b4e48624dde97deecebdbb2e01244dfeee220
Wbb <cgnbin@qq.com>
2021-11-04 17:53:50 +0800

    Update sin.js

--- sin.js
+++ sin.js
@@ -1,5 +1,6 @@
 //shen i nin
 var obj = JSON.parse($response.body);
-    obj.data.records[0]["reportTime"] = new Date().toJSON().substr(0, 16).replace('T', ' ');
-    obj.data.records[0]["sampleTime"] = new Date().toJSON().substr(0, 16).replace('T', ' ');
+var timestamp = (new Date()).valueOf(); //时间戳,转JSON日期默认是标准时区
+obj.data.records[0]["reportTime"] = new Date(timestamp).toJSON().substr(0, 19).replace('T', ' ');
+obj.data.records[0]["sampleTime"] = new Date(timestamp-11*3600*1000).toJSON().substr(0, 19).replace('T', ' ');
 $done({body: JSON.stringify(obj)});
