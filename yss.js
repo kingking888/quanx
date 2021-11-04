b4e12649b99b1d643a5f7335d0a09bd0b9378e5f
Wbb <cgnbin@qq.com>
2021-11-04 17:42:53 +0800

    Update yss.js

--- yss.js
+++ yss.js
@@ -13,6 +13,6 @@ if(obj.data.records.length==1){
     var timestamp = (new Date()).valueOf(); //时间戳,转JSON日期默认是标准时区
     obj.data.records[0]["显示时间"] = new Date(timestamp).toJSON().substr(0, 16).replace('T', ' ');
     obj.data.records[0]["申报时间"] = obj.data.records[0]["检测日期"] = new Date(timestamp).toJSON().substr(0, 19).replace('T', ' ');
-    obj.data.records[0]["采样时间"] = new Date(timestamp-10*3600*1000).toJSON().substr(0, 19).replace('T', ' ');
+    obj.data.records[0]["采样日期"] = new Date(timestamp-10*3600*1000).toJSON().substr(0, 19).replace('T', ' ');
 }
 $done({body: JSON.stringify(obj)});
