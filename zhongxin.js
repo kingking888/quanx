fe3ee28729caea435e208ded2cf11fa468e43e38
Wbb <cgnbin@qq.com>
2021-11-26 14:57:04 +0800

    Update zhongxin.js

--- zhongxin.js
+++ zhongxin.js
@@ -1,4 +1,4 @@
 var body = $response.body;
 body = body.replace("<stockNum>0</stockNum>","<stockNum>5</stockNum>");
-body = body.replace("1637823600","1637723600");
+body = body.replace(/<sale_begin_time>.*<\/sale_begin_time>/g,"<sale_begin_time>1637723600000</sale_begin_time>");
 $done({body});
