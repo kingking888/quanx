e2a51f69a9c9354c3947522b3f8df232447b7ae5
Wbb <cgnbin@qq.com>
2022-01-10 00:43:21 +0800

    Update ma_sin.js

--- ma_sin.js
+++ ma_sin.js
@@ -12,12 +12,12 @@ var timestamp = Math.round((new Date()).valueOf()/1000); //当前时间戳
 body.data.records[0]["reportTime"] = goodtime(new Date((timestamp-2*3600-15*60-22)*1000));
 body.data.records[0]["sampleTime"] = goodtime(new Date((get0(timestamp)-4*3600-25*60-35)*1000));
 
-body.data.records.push(body.data.records[0]);
+body.data.records.push(JSON.parse(JSON.stringify(body.data.records[0])));
 body.data.records[1]["reportTime"] = goodtime(new Date((timestamp-3*3600-13*60-36-86400)*1000));
 body.data.records[1]["sampleTime"] = goodtime(new Date((get0(timestamp)-6*3600-32*60-23-86400)*1000));
 
 
-body.data.records.push(body.data.records[0]);
+body.data.records.push(JSON.parse(JSON.stringify(body.data.records[0])));
 body.data.records[2]["reportTime"] = goodtime(new Date((timestamp-6*3600-25*60-28-172800)*1000));
 body.data.records[2]["sampleTime"] = goodtime(new Date((get0(timestamp)-7*3600-43*60-55-172800)*1000));
 
